667883abef0d710f677a6438e9da4f082a6bc214
Ainara Gil <ainaragilcm@gmail.com>
2025-01-31 09:20:54 +0100

    feature: maquetada seccion opiniones

--- src/app/components/main/opiniones/card-opinion/card-opinion.component.ts
+++ src/app/components/main/opiniones/card-opinion/card-opinion.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-opinion',
-  standalone: false,
-  
-  templateUrl: './card-opinion.component.html',
-  styleUrl: './card-opinion.component.css'
+    selector: 'app-card-opinion',
+    standalone: false,
+
+    templateUrl: './card-opinion.component.html',
+    styleUrl: './card-opinion.component.css',
 })
 export class CardOpinionComponent {
-
+    @Input() text: string = '';
+    @Input() photo: string = '';
+    @Input() name: string = '';
+    @Input() position: string = '';
 }
